bba8dda075da620591a0a6694275c118e15b5597
peterlloydskubur <63150386+llo7d@users.noreply.github.com>
2024-12-08 16:37:29 +0200

    Solution V1 (Simple if statement)

--- 13. Roman to Integer/code.py
+++ 13. Roman to Integer/code.py
@@ -1,23 +1,48 @@
 class Solution:
+    def romanToInt(s: str) -> int:
 
-    roman_values = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
+        # Initialize the roman values
+        roman_values = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
 
+        # Initialize the sum
+        sum = 0
 
+        # Loop through the string
+        for i in range(len(s)):
+            sum += roman_values[s[i]]
 
+            # If there is no next number, break the loop
+            if i+1 == len(s):
+                break
+
+            # If the current number is I, check if the next number is V or X
+            if s[i] == "I":
+                if s[i+1] == "V" or s[i+1] == "X":
+                    sum -= 2
+
+            # If the current number is X, check if the next number is L or C
+            if s[i] == "X":
+                if s[i+1] == "L" or s[i+1] == "C":
+                    sum -= 20
+
+            # If the current number is C, check if the next number is D or M
+            if s[i] == "C":
+                if s[i+1] == "D" or s[i+1] == "M":
+                    sum -= 200
+
+        # Return the sum
+        return sum
 
     
-
-    def romanToInt(self, s: str) -> int:
-        print(s)
+Solution.romanToInt("IVI")
 
 
 
-Solution().romanToInt("III")
