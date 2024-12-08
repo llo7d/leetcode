139ebe7d4ec995be28e97e84803a6c214fbb9ae3
peterlloydskubur <63150386+llo7d@users.noreply.github.com>
2024-12-08 16:09:22 +0200

    first commit

---
+++ 13. Roman to Integer/code.py
@@ -0,0 +1,23 @@
+class Solution:
+
+    roman_values = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
+
+
+
+    
+
+    def romanToInt(self, s: str) -> int:
+        print(s)
+
+
+
+Solution().romanToInt("III")
