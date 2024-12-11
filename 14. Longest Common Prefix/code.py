f11d6dc7fbc637e970e7498e91ee598fb1eadfaf
peterlloydskubur <63150386+llo7d@users.noreply.github.com>
2024-12-11 23:48:41 +0200

    Trying a new way, stil not there but something

--- 14. Longest Common Prefix/code.py
+++ 14. Longest Common Prefix/code.py
@@ -3,56 +3,36 @@ class Solution:
 
         prefix = ""
 
-        # if len(strs) == 1:
-        #     return strs[0]
+        position = 1
 
-        for i in range(len(strs)-1):
-            # Return the string with the shortest length
-            if len(strs[i]) < len(strs[i+1]):
-                prefix = strs[i]
-            else:
-                prefix = strs[i+1]
+        first_chars = []
 
-            for j in range(len(prefix)):
-                if prefix[j] == strs[i][j]:
-                    prefix = prefix[j]
-                else:
-                    prefix = ""
-                    return prefix
+        for i in range(len(strs)):
+            first_chars.append(strs[i][position])
+
+        print(first_chars)
+
+        # shortest = min(strs, key=len)
+
+        # for i in range(len(shortest)):
+        #     for j in range(len(strs)):
+        #         print(strs[j][i])
 
 
+    
+                
 
+                
         print(prefix)
-
-        
-        # This loops trough all the strings in the list
-        # for i in range(len(strs)): 
-        #     prefix_list.append(strs[i][:2])
-        #     # This loops trought individual strings
-        #     # for j in range(len(strs)):
-        #         # print(strs[i][:2])
-
-        # print(prefix_list)
-
-        # if len(prefix_list) == 1:
-        #     return prefix_list[0]
-        
-        # for i in range(len(prefix_list)-1): # Subtract 1 to avoid index out of range
-        #     if prefix_list[i] != prefix_list[i+1]:
-        #         prefix = ""
-        #         return prefix
-            
-        #     prefix = prefix_list[0]
-
-        # print(prefix)
-        # return prefix
+        return prefix
 
             
             
             
-# Solution().longestCommonPrefix(["flower","flow","flight"])
+Solution().longestCommonPrefix(["flower","flow","flight"])
 # Solution().longestCommonPrefix(["a"])
 # Solution().longestCommonPrefix(["","b"])
-Solution().longestCommonPrefix(["reflower","flow","flight"])
+# Solution().longestCommonPrefix(["ab", "a"]) # expected "a"
+# Solution().longestCommonPrefix(["reflower","flow","flight"])
 
 
